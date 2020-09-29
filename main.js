c91f9d027d52090be2164bc78fe1095f6184a4b4
akoganei <67255310+akoganei@users.noreply.github.com>
2020-09-29 14:51:27 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+{
+    class Post {
+        constructor(text) {
+            this.text　= text;
+            this.likeCount = 0;
+        }
+
+        show() {
+            console.log(`${this.text} - ${this.likeCount}likes`);
+        }
+
+        like() {
+            this.likeCount++;
+            this.show();
+        }
+    }
+    class SponsoredPost {
+        constructor(text, sponsor) {
+            this.text　= text;
+            this.likeCount = 0;
+            this.sponsor = sponsor;
+        }
+
+        show() {
+            console.log(`${this.text} - ${this.likeCount}likes`);
+            console.log(`...sponsored by ${this.sponsor}`);
+        }
+
+        like() {
+            this.likeCount++;
+            this.show();
+        }
+
+    }
+
+    const posts = [
+        new Post('JavaScriptの勉強中'),
+        new Post('プログラミング楽しい！'),
+        new SponsoredPost('3分で動画をマスターしよう！', 'dotinstall')
+    ]
+    posts[2].show();
+    posts[2].like();
+}
